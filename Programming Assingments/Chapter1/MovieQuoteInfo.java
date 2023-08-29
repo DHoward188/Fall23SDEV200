91710db263b170440b7dd2cb50727489170ef2e8
DHoward188 <143538025+DHoward188@users.noreply.github.com>
2023-08-29 17:10:06 -0400

    SDEV200 Class files

---
+++ Programming Assingments/Chapter1/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+public class MovieQuoteInfo {
+    public static void main(String[] args)
+    {
+        System.out.println("My favorite movie quote is \"It's Goku, but Broly, call me Kakarot\"");
+        System.out.println("The quote was from \"Dragon Ball Super: Broly\"");
+        System.out.println("The quote was said by Goku");
+        System.out.println("The movie was released in 2018");
+    }
+}
